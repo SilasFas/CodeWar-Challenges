6f49fdd12d4b651cdc995ab7151462a54d792414
Silas <silasfas@hotmail.com>
2023-02-15 03:55:05 -0300

    even-odd

---
+++ Even or odd number/index.js
@@ -0,0 +1,10 @@
+// Find a solution to calculate with the number is even or odd.
+
+function evenOrOdd(number) {
+    if (number % 2 === 0) {
+        return "even"
+    } else {
+        return "Odd"
+    }
+}
+console.log(evenOrOdd(1))
