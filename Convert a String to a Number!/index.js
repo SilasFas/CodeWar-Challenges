d00e0841e5ab1873502542f5219aea4f502fc25e
Silas <silasfas@hotmail.com>
2023-07-02 17:26:58 -0300

    Convert a String to a Number!

---
+++ Convert a String to a Number!/index.js
@@ -0,0 +1,12 @@
+/*
+We need a function that can transform a string into a number (integer).
+*/
+const stringToNumber = function (str) {
+  let number = Number(str)
+  return number;
+}
+
+
+console.log(stringToNumber('15'))
+
+
