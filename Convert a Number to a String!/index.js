4a7848a8688f36f97d39238c052dcd6797f7fb3e
Silas <silasfas@hotmail.com>
2023-07-02 17:20:50 -0300

    Convert a Number to a String

---
+++ Convert a Number to a String!/index.js
@@ -0,0 +1,15 @@
+/*
+We need a function that can transform a number (integer) into a string.
+*/
+function numberToString(number) {
+
+  let text = number
+  text = number.toString()
+  return text
+
+}
+
+
+console.log(numberToString(15))
+
+
